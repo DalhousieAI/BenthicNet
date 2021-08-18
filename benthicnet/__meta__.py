8c3f8caa300bf2965ce1208636910f8ae90b865d
Scott Lowe <scott.code.lowe@gmail.com>
2021-08-18 13:59:45 +0100

    Initialise project from template repository

---
+++ benthicnet/__meta__.py
@@ -0,0 +1,8 @@
+name = "benthicnet"
+path = name.lower().replace("-", "_").replace(" ", "_")
+version = "0.1.dev0"
+author = "Scott C. Lowe"
+author_email = "scott.code.lowe@gmail.com"
+description = "Dataset for underwater benthic imagery of the seafloor"
+url = "https://github.com/DalhousieAI/BenthicNet"
+license = "MIT"
